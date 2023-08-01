28feac8eb15284c3257d6b473910439c1d82e6dc
YuNandarMoe <yunandarmoe31@gmail.com>
2023-08-01 17:48:03 +0630

    created usertable and seeder

---
+++ database/seeders/UserTableSeeder.php
@@ -0,0 +1,52 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+
+class UserTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        DB::table('users')->insert([
+
+            // Admin
+            [
+                "full_name" => "Yu Nandar Moe Admin",
+                "username" => "Admin",
+                "email" => "admin@gmail.com",
+                "password" => Hash::make('111'),
+                "role" => "admin",
+                "status" => "active"
+            ],
+
+            // Vendor
+            [
+                "full_name" => "Yu Nandar Moe Vendor",
+                "username" => "Vendor",
+                "email" => "vendor@gmail.com",
+                "password" => Hash::make('111'),
+                "role" => "vendor",
+                "status" => "active"
+            ],
+
+             // Customer
+             [
+                "full_name" => "Yu Nandar Moe Customer",
+                "username" => "Customer",
+                "email" => "customer@gmail.com",
+                "password" => Hash::make('111'),
+                "role" => "customer",
+                "status" => "active"
+            ],
+        ]);
+    }
+}
