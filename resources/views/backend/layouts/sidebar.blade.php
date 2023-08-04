52bff66e239f68eb7ec3ef888381dc2cad414c03
YuNandarMoe <yunandarmoe31@gmail.com>
2023-08-04 19:45:32 +0630

    admin theme and menu items

---
+++ resources/views/backend/layouts/sidebar.blade.php
@@ -0,0 +1,249 @@
+    <!-- Sidebar -->
+    <ul class="navbar-nav bg-gradient-primary sidebar sidebar-dark accordion" id="accordionSidebar">
+        <!-- Sidebar - Brand -->
+        <a class="sidebar-brand d-flex align-items-center justify-content-center" href="index.html">
+            <div class="sidebar-brand-icon rotate-n-15">
+                <i class="fas fa-laugh-wink"></i>
+            </div>
+            <div class="sidebar-brand-text mx-3">SB Admin <sup>2</sup></div>
+        </a>
+        <!-- Divider -->
+        <hr class="sidebar-divider my-0">
+        <!-- Nav Item - Dashboard -->
+        <li class="nav-item active">
+            <a class="nav-link" href="index.html">
+                <i class="fas fa-fw fa-tachometer-alt"></i>
+                <span>Dashboard</span></a>
+        </li>
+        <!-- Divider -->
+        <hr class="sidebar-divider">
+        <!-- Heading -->
+        <div class="sidebar-heading">
+            Interface
+        </div>
+        <li class="nav-item">
+            <a class="nav-link btn" href="#collapseBanner" data-bs-toggle="collapse" role="button"
+                aria-expanded="false" aria-controls="collapseBanner">
+                <i class="fas fa-image"></i> <span>Banner Management</span>
+            </a>
+            <div class="collapse" id="collapseBanner">
+                <div class="bg-white py-2 collapse-inner rounded">
+                    <h6 class="collapse-header">Banner:</h6>
+                    <a class="collapse-item" href="buttons.html">Buttons</a>
+                    <a class="collapse-item" href="cards.html">Cards</a>
+                </div>
+            </div>
+        </li>
+        <li class="nav-item">
+            <a class="nav-link collapsed" href="#collapseCategory" data-bs-toggle="collapse" aria-expanded="false"
+                aria-controls="collapseCategory">
+                <i class="fas fa-sitemap"></i>
+                <span>Category Management</span>
+            </a>
+            <div id="collapseCategory" class="collapse">
+                <div class="bg-white py-2 collapse-inner rounded">
+                    <h6 class="collapse-header">Category:</h6>
+                    <a class="collapse-item" href="utilities-color.html">Colors</a>
+                    <a class="collapse-item" href="utilities-border.html">Borders</a>
+                    <a class="collapse-item" href="utilities-animation.html">Animations</a>
+                    <a class="collapse-item" href="utilities-other.html">Other</a>
+                </div>
+            </div>
+        </li>
+        <li class="nav-item">
+            <a class="nav-link collapsed" href="#collapseProducts" data-bs-toggle="collapse" aria-expanded="false"
+                aria-controls="collapseProducts">
+                <i class="fas fa-briefcase"></i>
+                <span>Products Management</span>
+            </a>
+            <div id="collapseProducts" class="collapse">
+                <div class="bg-white py-2 collapse-inner rounded">
+                    <h6 class="collapse-header">Products:</h6>
+                    <a class="collapse-item" href="utilities-color.html">Colors</a>
+                    <a class="collapse-item" href="utilities-border.html">Borders</a>
+                    <a class="collapse-item" href="utilities-animation.html">Animations</a>
+                    <a class="collapse-item" href="utilities-other.html">Other</a>
+                </div>
+            </div>
+        </li>
+        <li class="nav-item">
+            <a class="nav-link btn" href="#collapseCarts" data-bs-toggle="collapse" role="button" aria-expanded="false"
+                aria-controls="collapseCarts">
+                <i class="fas fa-shopping-cart"></i>
+                <span>Carts Management</span>
+            </a>
+            <div class="collapse" id="collapseCarts">
+                <div class="bg-white py-2 collapse-inner rounded">
+                    <h6 class="collapse-header">Carts:</h6>
+                    <a class="collapse-item" href="buttons.html">Buttons</a>
+                    <a class="collapse-item" href="cards.html">Cards</a>
+                </div>
+            </div>
+        </li>
+        <li class="nav-item">
+            <a class="nav-link btn" href="#collapseOrder" data-bs-toggle="collapse" role="button" aria-expanded="false"
+                aria-controls="collapseOrder">
+                <i class="fas fa-store"></i>
+                <span>Order Management</span>
+            </a>
+            <div class="collapse" id="collapseOrder">
+                <div class="bg-white py-2 collapse-inner rounded">
+                    <h6 class="collapse-header">Order:</h6>
+                    <a class="collapse-item" href="buttons.html">Buttons</a>
+                    <a class="collapse-item" href="cards.html">Cards</a>
+                </div>
+            </div>
+        </li>
+        <li class="nav-item">
+            <a class="nav-link btn" href="#collapsePostCategory" data-bs-toggle="collapse" role="button"
+                aria-expanded="false" aria-controls="collapsePostCategory">
+                <i class="fas fa-sitemap"></i> 
+                <span>Post Category</span>
+            </a>
+            <div class="collapse" id="collapsePostCategory">
+                <div class="bg-white py-2 collapse-inner rounded">
+                    <h6 class="collapse-header">PostCategory:</h6>
+                    <a class="collapse-item" href="buttons.html">Buttons</a>
+                    <a class="collapse-item" href="cards.html">Cards</a>
+                </div>
+            </div>
+        </li>
+        <li class="nav-item">
+            <a class="nav-link btn" href="#collapsePostTag" data-bs-toggle="collapse" role="button"
+                aria-expanded="false" aria-controls="collapsePostTag">
+                <i class="fas fa-tags"></i>                
+                <span>Post Tag</span>
+            </a>
+            <div class="collapse" id="collapsePostTag">
+                <div class="bg-white py-2 collapse-inner rounded">
+                    <h6 class="collapse-header">Post Tag:</h6>
+                    <a class="collapse-item" href="buttons.html">Buttons</a>
+                    <a class="collapse-item" href="cards.html">Cards</a>
+                </div>
+            </div>
+        </li>
+        <li class="nav-item">
+            <a class="nav-link btn" href="#collapsePostManagement" data-bs-toggle="collapse" role="button"
+                aria-expanded="false" aria-controls="collapsePostManagement">
+                <i class="fas fa-tasks"></i>
+                <span>Post Management</span>
+            </a>
+            <div class="collapse" id="collapsePostManagement">
+                <div class="bg-white py-2 collapse-inner rounded">
+                    <h6 class="collapse-header">Post Management:</h6>
+                    <a class="collapse-item" href="buttons.html">Buttons</a>
+                    <a class="collapse-item" href="cards.html">Cards</a>
+                </div>
+            </div>
+        </li>
+        <li class="nav-item">
+            <a class="nav-link btn" href="#collapseReview" data-bs-toggle="collapse" role="button"
+                aria-expanded="false" aria-controls="collapseReview">
+                <i class="far fa-star"></i>          
+                <span>Review Management</span>
+            </a>
+            <div class="collapse" id="collapseReview">
+                <div class="bg-white py-2 collapse-inner rounded">
+                    <h6 class="collapse-header">Review Management:</h6>
+                    <a class="collapse-item" href="buttons.html">Buttons</a>
+                    <a class="collapse-item" href="cards.html">Cards</a>
+                </div>
+            </div>
+        </li>
+        <li class="nav-item">
+            <a class="nav-link btn" href="#collapseCoupon" data-bs-toggle="collapse" role="button"
+                aria-expanded="false" aria-controls="collapseCoupon">
+                <i class="fas fa-ticket-alt"></i>            
+                <span>Coupon Management</span>
+            </a>
+            <div class="collapse" id="collapseCoupon">
+                <div class="bg-white py-2 collapse-inner rounded">
+                    <h6 class="collapse-header">Coupon Management:</h6>
+                    <a class="collapse-item" href="buttons.html">Buttons</a>
+                    <a class="collapse-item" href="cards.html">Cards</a>
+                </div>
+            </div>
+        </li>
+        <li class="nav-item">
+            <a class="nav-link btn" href="#collapseUser" data-bs-toggle="collapse" role="button"
+                aria-expanded="false" aria-controls="collapseUser">
+                <i class="fas fa-users"></i>      
+                <span>User Management</span>
+            </a>
+            <div class="collapse" id="collapseUser">
+                <div class="bg-white py-2 collapse-inner rounded">
+                    <h6 class="collapse-header">User Management:</h6>
+                    <a class="collapse-item" href="buttons.html">Buttons</a>
+                    <a class="collapse-item" href="cards.html">Cards</a>
+                </div>
+            </div>
+        </li>
+        <li class="nav-item">
+            <a class="nav-link btn" href="#collapseComment" data-bs-toggle="collapse" role="button"
+                aria-expanded="false" aria-controls="collapseComment">
+                <i class="fas fa-comments"></i>
+                <span>Comment Management</span>
+            </a>
+            <div class="collapse" id="collapseComment">
+                <div class="bg-white py-2 collapse-inner rounded">
+                    <h6 class="collapse-header">Comment Management:</h6>
+                    <a class="collapse-item" href="buttons.html">Buttons</a>
+                    <a class="collapse-item" href="cards.html">Cards</a>
+                </div>
+            </div>
+        </li>
+        <!-- Divider -->
+        <hr class="sidebar-divider">
+        <!-- Heading -->
+        <div class="sidebar-heading">
+            Addons
+        </div>
+        <!-- Nav Item - Pages Collapse Menu -->
+        <li class="nav-item">
+            <a class="nav-link collapsed" href="#" data-toggle="collapse" data-target="#collapsePages"
+                aria-expanded="true" aria-controls="collapsePages">
+                <i class="fas fa-fw fa-folder"></i>
+                <span>Pages</span>
+            </a>
+            <div id="collapsePages" class="collapse" aria-labelledby="headingPages" data-parent="#accordionSidebar">
+                <div class="bg-white py-2 collapse-inner rounded">
+                    <h6 class="collapse-header">Login Screens:</h6>
+                    <a class="collapse-item" href="login.html">Login</a>
+                    <a class="collapse-item" href="register.html">Register</a>
+                    <a class="collapse-item" href="forgot-password.html">Forgot Password</a>
+                    <div class="collapse-divider"></div>
+                    <h6 class="collapse-header">Other Pages:</h6>
+                    <a class="collapse-item" href="404.html">404 Page</a>
+                    <a class="collapse-item" href="blank.html">Blank Page</a>
+                </div>
+            </div>
+        </li>
+        <!-- Nav Item - Charts -->
+        <li class="nav-item">
+            <a class="nav-link" href="charts.html">
+                <i class="fas fa-fw fa-chart-area"></i>
+                <span>Charts</span></a>
+        </li>
+        <!-- Nav Item - Tables -->
+        <li class="nav-item">
+            <a class="nav-link" href="tables.html">
+                <i class="fas fa-fw fa-table"></i>
+                <span>Tables</span></a>
+        </li>
+        <!-- Divider -->
+        <hr class="sidebar-divider d-none d-md-block">
+        <!-- Sidebar Toggler (Sidebar) -->
+        <div class="text-center d-none d-md-inline">
+            <button class="rounded-circle border-0" id="sidebarToggle"></button>
+        </div>
+        <!-- Sidebar Message -->
+        <div class="sidebar-card d-none d-lg-flex">
+            <img class="sidebar-card-illustration mb-2" src="{{ asset('backend/img/undraw_rocket.svg') }}"
+                alt="...">
+            <p class="text-center mb-2"><strong>SB Admin Pro</strong> is packed with premium features, components,
+                and more!</p>
+            <a class="btn btn-success btn-sm" href="https://startbootstrap.com/theme/sb-admin-pro">Upgrade to
+                Pro!</a>
+        </div>
+    </ul>
+    <!-- End of Sidebar -->
